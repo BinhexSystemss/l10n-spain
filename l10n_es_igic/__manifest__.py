ae3a4545e0c76480d3321f12e9acc3ba626b27c3
Christian-RB <c.ramos@binhex.es>
2025-02-04 12:27:53 +0000

    [MIG] l10n_es_igic: Migration to 18.0

--- l10n_es_igic/__manifest__.py
+++ l10n_es_igic/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     "name": "IGIC (Impuesto General Indirecto Canario)",
-    "version": "17.0.2.1.0",
+    "version": "18.0.1.0.0",
     "countries": ["es"],
     "author": "David Diz Martínez,"
     "Atlantux Consultores - Enrique Zanardi,"
